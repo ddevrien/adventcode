7f4eebffc1630cd63f2a3391b19683543348bda6
Dennis Devriendt <dennis@devriendt.eu>
2015-12-05 16:32:49 +0100

    Day 1: Not Quite Lisp

---
+++ src/main/java/be/devriendt/advent/Day1FloorCounter.java
@@ -0,0 +1,44 @@
+package be.devriendt.advent;
+
+/**
+ * Created by Dennis on 5/12/2015.
+ */
+public class Day1FloorCounter {
+
+    private static final char UP = '(';
+
+    private static final char DOWN = ')';
+
+    public static int directionToFloor(String input) {
+        int floor = 0;
+        for (char c: input.toCharArray()) {
+            if (c == UP) {
+                floor++;
+            } else if (c == DOWN) {
+                floor--;
+            } else {
+                throw new IllegalArgumentException("Unexpected symbol: " + c);
+            }
+        }
+        return floor;
+    }
+
+    public static int findBasementEnterPosition(String input) {
+        int floor = 0;
+        char[] chars = input.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (c == UP) {
+                floor++;
+            } else if (c == DOWN) {
+                floor--;
+                if (floor == -1) {
+                    return ++i;
+                }
+            } else {
+                throw new IllegalArgumentException("Unexpected symbol: " + c);
+            }
+        }
+        return floor;
+    }
+}
